c7400ec3831b9dc101544c1fca6d6812a8413095
surendra <surendra.chakali@hyniva.com>
2023-10-12 16:47:04 +0530

    get changes

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 // This program is for getting the employee bank details based http GET method.
 const {
   DynamoDBClient, // Dynamodb instance
+  GetItemCommand,
   PutItemCommand,
   UpdateItemCommand,
 } = require("@aws-sdk/client-dynamodb"); //aws-sdk is used to build rest APIs
@@ -153,7 +154,7 @@ const updateEmployeeSalary = async (event) => {
     const body = JSON.parse(event.body);
     const empId = event.pathParameters.empId;
 
-    const existingEmployee = await getEmployeeByEmpId(empId);
+    const existingEmployee = await getEmployeeSalary(empId);
     if (!existingEmployee) {
       response.statusCode = 404;
       response.body = JSON.stringify({
@@ -214,7 +215,44 @@ const updateEmployeeSalary = async (event) => {
   }
   return response;
 };
+
+const getEmployeeSalary = async (event) => {
+  let response = { statusCode: 200 };
+  try{
+const params = {
+  TableName: process.env.DYNAMODB_TABLE_NAME, // Getting table name from the servetless.yml and setting to the TableName
+  Key: marshall({ empId: event.pathParameters.empId }), // Convert a JavaScript object into a DynamoDB record.
+};
+//await response from db when sent getItem command with params
+//containing tablename, key and only display empId and bank details
+const { Item } = await client.send(new GetItemCommand(params)); //An asynchronous call to DynamoDB to retrieve an item
+console.log({ Item });
+if (!Item) {
+  // If there is no employee bank details found
+  response.statusCode = 404; // Setting the status code to 404
+  response.body = JSON.stringify({
+    message: "Employee bank details not found.",
+  }); // Setting error message
+} else {
+  // If employee bank details found in the dynamoDB set to data
+  response.body = JSON.stringify({
+    message: "Successfully retrieved Employee bank details.",
+    data: unmarshall(Item), // A DynamoDB record into a JavaScript object and setting to the data
+  });
+}
+} catch (e) {
+// If any errors will occurred
+console.error(e);
+response.body = JSON.stringify({
+  statusCode: e.statusCode, // Set server side status code
+  message: "Failed to retrieved employee bank details.",
+  errorMsg: e.message, // Set error message
+});
+}
+return response;
+}
 module.exports = {
+  getEmployeeSalary,
   createEmployeeSalary,
   updateEmployeeSalary,
 }; // exporting the function
